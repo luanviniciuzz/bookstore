261db67d5437f6830800eb262236c8828862151e
Luan Vinícius <luanviniciuzz@gmail.com>
2024-09-23 17:50:29 -0300

    nav bar e listagem de livros estatico

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React  from 'react'
 import Navbar from './components/Navbar/Navbar'
+import BooksMenu from './components/BooksMenu/BooksMenu'
 
 function App() {
 
   return (
     <div className="overflow-x-hidden">
         <Navbar/>
+        <BooksMenu/>
     </div>
   )
 }
