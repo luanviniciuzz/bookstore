954e64976e48f7cae5da1733db1e74465a02e57f
Luan Vinícius <luanviniciuzz@gmail.com>
2024-09-23 15:44:15 -0300

    iniciando projeto frontend

---
+++ frontend/src/mockData/data.js
@@ -0,0 +1,22 @@
+export const NavbarMenu = [
+  {
+    id: 1,
+    title: "Início",
+    link: "",
+  },
+  {
+    id: 2,
+    title: "Quem somos",
+    link: "#",
+  },
+  {
+    id: 3,
+    title: "Blog",
+    link: "#",
+  },
+  {
+    id: 4,
+    title: "Contato",
+    link: "#",
+  },
+]
