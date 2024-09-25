7f9c7059d6cb8913502a4f699bbb602c4e413bc2
Luan Vinícius <luanviniciuzz@gmail.com>
2024-09-25 18:35:28 -0300

    Componentes adcionais de homescreen

--- frontend/src/srcreens/HomeScreen.jsx
+++ frontend/src/srcreens/HomeScreen.jsx
@@ -1,33 +1,49 @@
 import React, {useState, useEffect} from 'react'
 import { Row, Col } from 'react-bootstrap'
+
+import { useDispatch, useSelector } from 'react-redux'
+import { listProducts } from '../actions/productActions'
+
 import Product from '../components/Product'
-import axios from 'axios'
-import API from '../services/API'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import Paginate from '../components/Paginate'
+import ProductCarousel from '../components/ProductCarousel'
+import { useParams } from 'react-router-dom'
 
 function HomeScreen(){
-  const [products, setProducts] = useState([])
+
+  const history = useParams()
+  const dispatch = useDispatch()
+  const productList = useSelector(state => state.productList)
+  const { error, loading, products, page, pages } = productList
+
+  let keyword = ''
 
   useEffect(() => {
+      dispatch(listProducts(keyword))
 
-    async function fetchProducts(){
-      const{ data } = await API.get('/api/products/')
-      setProducts(data)
-    }
-    fetchProducts()
-  },[])
+  }, [dispatch])
 
   return (
     <div>
-       <h1>Latest Products</h1>
-          <div>
-              <Row>
-                  {products.map(p => (
-                      <Col key={p._id} sm={12} md={6} lg={4} xl={3}>
-                          <Product product={p}/>
-                      </Col>
-                  ))}
-              </Row>
-          </div>
+      {!keyword && <ProductCarousel />}
+
+          <h1>Latest Products</h1>
+          {loading ? <Loader />
+              : error ? <Message variant='danger'>{error}</Message>
+                  :
+                  <div>
+                      <Row>
+                          {products.map(product => (
+                              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                                  <Product product={product} />
+                              </Col>
+                          ))}
+                      </Row>
+                      <Paginate page={page} pages={pages} keyword={keyword} />
+                  </div>
+          }"
     </div>
   )
 }
