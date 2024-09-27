d49c98f35b9b838791668cbf5fbd424480f915d1
Luan Vinícius <luanviniciuzz@gmail.com>
2024-09-27 12:18:25 -0300

    configurando postgresql

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -115,12 +115,23 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql',
+          'NAME': 'bookstore',
+          'USER': 'postgres',
+          'PASSWORD': 'postgreSQL1z2x3c',
+          'HOST': 'localhost',
+          'PORT': '5432',
+     }
+  }
 
 
 # Password validation
